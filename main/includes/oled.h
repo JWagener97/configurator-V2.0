2386e80b80fecbd7f442cb93f66abae95749ec7b
Jonathan Wagener <Jonathan.Wagener@cbi-electric.com>
2023-06-02 11:42:17 +0200

    Created oled.h file
    Header file for the oled.c file

---
+++ main/includes/oled.h
@@ -0,0 +1,19 @@
+/**
+ * @file oled.h
+ * @author Jonathan Wagener
+ * @brief System file that contains all the functions pertaing to running the oled display
+ * @version 0.1
+ * @date 2023-06-02
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef _oled_H
+#define _oled_H
+
+#include <stdint.h>
+
+void oled_setup(void);
+
+#endif
