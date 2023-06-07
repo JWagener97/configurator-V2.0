3ad28fcf6145213c62a6eba5ef0795fd3e536679
Jonathan <jonathan.wagener@cbi-electric.com>
2023-06-07 09:44:44 +0200

    Created the I2C header file

---
+++ main/includes/I2C.h
@@ -0,0 +1,22 @@
+/**
+ * @file I2C.h
+ * @author Jonathan Wagener
+ * @brief System file that contains all the functions pertaing to running the oled display and reading the information off the MAX17043
+ * @version 0.1
+ * @date 2023-06-02
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef _I2C_H
+#define _I2C_H
+
+#include <stdint.h>
+
+void oled_setup(void);
+void oled_display(void);
+
+
+
+#endif
