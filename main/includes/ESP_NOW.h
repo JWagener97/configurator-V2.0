52416a9accb17e3d1d32ffabaa51288af0775cce
Jonathan <jonathan.wagener@cbi-electric.com>
2023-06-07 09:42:20 +0200

    Added the ESPNOW C file

---
+++ main/includes/ESP_NOW.h
@@ -0,0 +1,20 @@
+/**
+ * @file I2C.h
+ * @author Jonathan Wagener
+ * @brief System file that contains all the functions pertaing to running ESP Now
+ * @version 0.1
+ * @date 2023-06-02
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef _ESP_Now_H
+#define _ESP_Now_H
+
+#include <stdint.h>
+
+
+
+
+#endif
